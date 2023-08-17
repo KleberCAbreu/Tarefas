539d4117a52a111010614a4389154e08c0428673
Kleber Clemente de Abreu <kleber.abreu@etec.sp.gov.br>
2023-08-17 13:18:07 -0300

    Arquivos Modificados

--- lib/tarefa_page.dart
+++ lib/tarefa_page.dart
@@ -7,12 +7,34 @@ class TarefasPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Tarefas")),
+      body: listaVazia(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushNamed("/add");
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  listaVazia() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset("imagens/lista.jpg"),
+            ),
+            Text(
+              "Lista de tarefas vazia!",
+              style: TextStyle(fontSize: 24),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
