04ea3ee05f358e8896eaf3a831ff0a8729461a99
Kleber <kleber.abreu@etec.sp.gov.br>
2023-10-16 15:01:21 -0300

    .

--- lib/app.dart
+++ lib/app.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:tarefas/tarefa_model.dart';
-import 'package:tarefas/tarefa_page.dart';
 import 'package:tarefas/tarefa_state.dart';
-import 'package:tarefas/tarefas_edit.dart';
 import 'package:tarefas/tarefas_helper.dart';
 import 'package:tarefas/tarefas_helper.impl.dart';
 import 'package:tarefas/tarefas_novo.dart';
+import 'package:tarefas/tarefas_page.dart';
+
+import 'tarefas_edit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TarefaState state = TarefaState();
     TarefasHelper helper = TarefasHelperImpl(); 
-
+   TarefaState state = TarefaState(helper);
+ 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.lime),
+      theme: ThemeData(primarySwatch: Colors.orange),
       initialRoute: "/",
-      routes: {"/":(context)=>TarefasPage(helper: helper,),
+      routes: {"/":(context)=>TarefasPage(state:state),
                "/add":(context)=>TarefasNovo(state: state,helper: helper,),
-                "/edit":(context) {
-                  state.tarefa = ModalRoute.of(context)!.settings.arguments as Tarefa;
-                  return TarefasEdit(state: state,helper: helper,);
-                }
-                },
+               "/edit":(context) {
+                 state.tarefa = ModalRoute.of(context)!.settings.arguments as Tarefa;
+                 return TarefasEdit(state: state,helper: helper,);
+               }},
       
     );
   }
